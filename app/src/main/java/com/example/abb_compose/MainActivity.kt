5d449c243ff9e732462ac30f59b75c7cf3198e5f
Firuza Aliyeva <firuza.aliyeva@procure.az>
2023-02-01 15:30:20 +0400

    HW10 Task 3: Custom Grid Layout

--- app/src/main/java/com/example/abb_compose/MainActivity.kt
+++ app/src/main/java/com/example/abb_compose/MainActivity.kt
@@ -13,13 +13,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.abb_compose.ui.theme.ABB_ComposeTheme
+import java.lang.Math.ceil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ABB_ComposeTheme {
-                MyList()
+                MyGrid()
 
             }
         }
@@ -31,8 +32,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ABB_ComposeTheme {
-        MyList()
+        MyGrid()
     }
+}
+
+
+@Composable
+fun MyGrid() {
+    SimpleColumn() {
+        Text(text = "MY Custom Grid", Modifier.padding(10.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        val list = listOf<String>(
+            "1", "2", "3", "meow", "4", "5", "6", "7",
+            "meow", "8", "9", "10", "meow", "8", "9", "10", "9", "10", "asdasdasdasdasd"
+        )
+        SimpleGrid(columnsMax = 2) {
+            list.forEach { i -> MyListItem(content = i) }
+
+        }
+    }
+
 }
 
 
@@ -110,6 +129,56 @@ fun SimpleRow(
             for (placeable in placeables) {
                 placeable.placeRelative(x = x, y = 0)
                 x += placeable.width
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SimpleGrid(
+    modifier: Modifier = Modifier,
+    columnsMax: Int,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { m -> m.measure(constraints) }
+
+        val height =
+            placeables.filterIndexed { index, _ -> ((index) % columnsMax) == 0 }.sumOf { it.height }
+
+        val rowLengths = mutableListOf<Int>()
+        val rows = kotlin.math.ceil((placeables.size / columnsMax.toDouble())).toInt()
+        for (r in 1..rows) {
+            val rowStartIndex = (r - 1) * columnsMax
+            var rowEndIndex = rowStartIndex + columnsMax
+            if (rowEndIndex > placeables.size ) {
+                rowEndIndex = placeables.size
+            }
+            val rowPlaceables = placeables.subList(rowStartIndex, rowEndIndex)
+            rowLengths.add(rowPlaceables.sumOf { it.width })
+        }
+        val width = rowLengths.maxOf { it }
+        var col = 0
+
+        layout(width, height) {
+            var x = 0
+            var y = 0
+            for (placeable in placeables) {
+
+                if (col == columnsMax) {
+                    col = 0
+                    x = 0
+                    y += placeable.height
+                }
+                placeable.placeRelative(x = x, y = y)
+                x += placeable.width
+                col++
+
+
             }
         }
     }
